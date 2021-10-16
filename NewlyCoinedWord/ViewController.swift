56c5f82661f7394cd97ae8d80d034bfce4ca31e3
STARIMK <starimk@naver.com>
2021-10-16 21:22:16 +0900

    Initial Commit

---
+++ NewlyCoinedWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewlyCoinedWord
+//
+//  Created by 심민규 on 2021/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
